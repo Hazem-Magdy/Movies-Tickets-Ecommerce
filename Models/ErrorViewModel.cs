e57b2c9d50a4e8f02eeaf4a1e387f8eccbb2fad1
Hazem magdy <hazemmagdy545@gmail.com>
2023-04-28 21:38:44 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Movies_Tickets_Ecommerce_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
